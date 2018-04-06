2c500d8c7b6c2b00daad38c240612833be7a9a46
niraj <nirajkat786@gmail.com>
2018-04-06 21:04:34 +0530

    pasing installed firefox driver and removed dependency of chrome driver as it was throwing error reg some 32 bit

--- test/java/TestScripts/com_kayak_createNewAccount.java
+++ test/java/TestScripts/com_kayak_createNewAccount.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
@@ -18,13 +19,12 @@ public class com_kayak_createNewAccount {
   @Test
 public void  driverInit() throws IOException {
 	  
-	 System.setProperty("webdriver.chrome.driver","C:\\Users\\Rathore\\.m2\\repository\\org\\seleniumhq\\selenium\\selenium-chrome-driver\\3.11.0\\selenium-chrome-driver-3.11.0.jar");
+	 System.setProperty("webdriver.gecko.driver","C:\\Users\\Rathore\\eclipse-workspace\\Kayak001\\src\\geckodriver.exe");
 	 System.out.println("System property is set");
-	  driver=new ChromeDriver();
+	  driver=new FirefoxDriver();
 	  driver.manage().timeouts().implicitlyWait(600, TimeUnit.SECONDS);
 	  WebDriverWait wait = new WebDriverWait(driver, 36000);
 	  wait.until(ExpectedConditions.urlToBe("https://www.kayak.co.in"));
-	  
 	  com_kayak_driver_Init hp= new com_kayak_driver_Init();
 	  hp.kayak_hmPage(driver);
   }
